af9d17a3bbe1879929d7764e2401c806cd18568a
Arshiyatabassun <Arshiyatabassum30@gmail.com>
2022-01-22 09:45:25 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import react from 'react';
+import AgeCal from './components/AgeCal';
+
+function App() {
+  return (
+    <div className="container">
+      <AgeCal/>
+    </div>
+  );
+}
+
+export default App;
